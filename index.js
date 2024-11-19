3fa15afacc8d1337ae6c00eae66af93d78d644f8
Matthieu Berger <104015028+BERGER-Matthieu@users.noreply.github.com>
2024-11-19 11:53:32 +0100

    style + script routes

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/Home', (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
 
+app.get('/script/:scriptName', (req, res) => {
+    res.sendFile(__dirname + `/public/script/${req.params.scriptName}.js`)
+})
+
+app.get('/style/:styleName', (req, res) => {
+    res.sendFile(__dirname + `/public/style/${req.params.styleName}.js`)
+})
+
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
