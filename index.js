d686b92c191cfbe5486a5e4cb8f366ff419b21c4
Matthieu Berger <104015028+BERGER-Matthieu@users.noreply.github.com>
2024-11-19 13:12:52 +0100

    save home

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const express = require('express')
+import express from "express"
+import __dirname from "node:path"
 const app = express()
 const port = 3000
 
-app.get('/Home', (req, res) => {
-    res.sendFile(__dirname + "/views/home.html")
-})
-
+//File routes
 app.get('/script/:scriptName', (req, res) => {
-    res.sendFile(__dirname + `/public/script/${req.params.scriptName}.js`)
+    console.log(__dirname + `/public/script/${req.params.scriptName}.js`)
+    //res.sendFile(__dirname + `/public/script/${req.params.scriptName}.js`)
 })
 
 app.get('/style/:styleName', (req, res) => {
     res.sendFile(__dirname + `/public/style/${req.params.styleName}.js`)
 })
 
+//View routes
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
 
-
+//Start
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- public/script/home.js
+++ public/script/home.js
@@ -1,1 +1,10 @@
-console.log("caca")
+import { v4 as uuidv4 } from 'uuid';
+
+const key = `task_${uuidv4}`
+const value = {
+    title : "Courses",
+    text : "Oeuf, lait ...",
+    isDone : false
+}
+
+localStorage.setItem(key, value)
